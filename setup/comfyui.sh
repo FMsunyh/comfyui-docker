3443343f40f345a96f19a0f07395efcac40776ed
Firmin.sun <fmsunyh@gmail.com>
2025-04-21 17:59:11 +0800

    添加模型Flux-Dev-4-step

--- setup/comfyui.sh
+++ setup/comfyui.sh
@@ -19,6 +19,7 @@ CLIP_DIR="$BASE_DIR/volumes/$APP/data/models/clip"
 VAE_DIR="$BASE_DIR/volumes/$APP/data/models/vae"
 DIFFUSION_MODELS_DIR="$BASE_DIR/volumes/$APP/data/models/diffusion_models"
 UPSCALE_MODELS_DIR="$BASE_DIR/volumes/$APP/data/models/upscale_models"
+LORAS_DIR="$BASE_DIR/volumes/$APP/data/models/loras"
 
 # ========== 工具函数 ==========
 
@@ -58,6 +59,7 @@ create_dir_if_not_exists "$CLIP_DIR"
 create_dir_if_not_exists "$VAE_DIR"
 create_dir_if_not_exists "$DIFFUSION_MODELS_DIR"
 create_dir_if_not_exists "$UPSCALE_MODELS_DIR"
+create_dir_if_not_exists "$LORAS_DIR"
 
 # 下载文件列表
 # download_if_not_exists "$VAE_DIR"/wan_2.1_vae.safetensors "https://huggingface.co/Comfy-Org/Wan_2.1_ComfyUI_repackaged/resolve/main/split_files/vae/wan_2.1_vae.safetensors"
@@ -79,6 +81,8 @@ download_if_not_exists "$UPSCALE_MODELS_DIR"/8x_NMKD-Typescale_175k.pth "https:/
 download_if_not_exists "$UPSCALE_MODELS_DIR"/8x_NMKD-Superscale_150000_G.pth "https://huggingface.co/uwg/upscaler/resolve/main/ESRGAN/8x_NMKD-Superscale_150000_G.pth"
 download_if_not_exists "$UPSCALE_MODELS_DIR"/4x_NMKD-Superscale-SP_178000_G.pth "https://huggingface.co/uwg/upscaler/resolve/main/ESRGAN/4x_NMKD-Superscale-SP_178000_G.pth"
 download_if_not_exists "$UPSCALE_MODELS_DIR"/4x_NMKD-Siax_200k.pth "https://huggingface.co/gemasai/4x_NMKD-Siax_200k/resolve/main/4x_NMKD-Siax_200k.pth"
+
+download_if_not_exists "$LORAS_DIR"/Flux-Dev-4-step.safetensors "https://liblibai-online.liblib.cloud/web/model/84cd9ecbef5b4333afb1125200e50242/c41f8106c9f02c94f89c0fbad50f143409b89d5007916e573cda39b78a3d8a37.safetensors?auth_key=1745231116-305efad2866e4e458f501a0399f8c6fc-0-113ca53dd39d41bbed413f6e324694d4&attname=Flux%20Dev%E6%A8%A1%E5%9E%8B4%E6%AD%A5%E5%87%BA%E5%9B%BElora_Flux%20Dev%E6%A8%A1%E5%9E%8B4%E6%AD%A5%E5%87%BA%E5%9B%BElora_Flux%20Dev%204-step.safetensors"
 
 # 等待所有后台任务完成
 wait
