df0f6d85e9523c0ed2a3e5af3656183af57ea734
Firmin.sun <fmsunyh@gmail.com>
2025-04-23 17:12:38 +0800

    新增插件

--- setup/clone_or_pull_custom_nodes.sh
+++ setup/clone_or_pull_custom_nodes.sh
@@ -15,6 +15,9 @@ TARGET_DIR="$BASE_DIR/volumes/$APP/data/custom_nodes"
 
 # 仓库地址列表（请替换为你真实的10个仓库）
 REPOS=(
+  "git@github.com:talesofai/comfyui-browser.git"
+  "git@github.com:11cafe/comfyui-workspace-manager.git"
+
   "git@github.com:jax-explorer/ComfyUI-UNO.git"
   
   "git@github.com:Comfy-Org/ComfyUI-Manager.git"
